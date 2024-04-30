a8f77e206bc819a7ecd11d25f3d342e2c1e4e83c
x86skwizer <yassineamrire@protonmail.com>
2024-04-30 21:59:46 +0100

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/7-main.js
@@ -0,0 +1,6 @@
+mport hasValuesFromArray from "./7-has_array_values.js";
+
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
+
