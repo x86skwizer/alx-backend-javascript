4f4be9d1e937b5074c18d424c5c3ba1a1773cf33
x86skwizer <yassineamrire@protonmail.com>
2024-06-18 11:19:47 +0000

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints in STDOUT the string argument
+ * @param {String} msg message to be printed
+ */
+const displayMessage = (msg) => {
+	console.log(msg);
+}
+
+module.exports = displayMessage;
